4ab4ba0f606ff111d35a7700b6ff741e3f078791
Navaharish <navaharish2005@gmail.com>
2023-08-27 13:57:52 +0530

    c

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Cards from './Cards'
 
 const Product = () => {
     return (
         <div>
             <h1 className='font-bold text-5xl font-sans tracking-normal'>PRODUCT</h1>
             <div className=' bg-gradient-to-r from-red to-blue  h-2 w-40 rounded-full'></div>
-
+            <Cards />
         </div>
     )
 }
